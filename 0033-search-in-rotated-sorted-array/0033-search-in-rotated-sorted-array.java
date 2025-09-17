45d188e583781eba5fd91da51bcc338e528f382b
Mohammed Aadil <111aadilofficial@gmail.com>
2025-09-17 14:30:45 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
         if(nums.length==1){
-            if(nums[0] == target) return 0;
+            if(nums[0]==target) return 0;
             else return -1;
         }
-        int i=0;
-        int index=0;
-        while(i<nums.length-1){
-            if(nums[index]<nums[index+1]) index++;
-            else break;
-            i++;
-        }
-        int left=manualBinarySearch(nums , target , 0 ,index);
-        int right=manualBinarySearch(nums , target , index+1 ,nums.length-1);
-        if(left != -1) return left;
-        else if(right != -1) return right;
-        return -1;
+
+        int pivot = findPivot(nums);
+
+        if(pivot==-1) return manualBinarySearch(nums , target , 0 , nums.length-1);
+
+        if(nums[pivot]==target) return pivot;
+
+        if(target >= nums[0]) return manualBinarySearch(nums , target , 0 , pivot-1);
+        
+        return manualBinarySearch(nums , target , pivot+1, nums.length-1);
     }
     public int manualBinarySearch(int[] nums , int target ,int start , int end ){
         while(start<=end){
@@ -23,6 +21,18 @@ class Solution {
             if(target>nums[mid]) start = mid+1;
             else if(target<nums[mid]) end = mid-1;
             else return mid;
+        }
+        return -1;
+    }
+    public int findPivot(int[] nums){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start + (end-start)/2;
+            if(mid<end && nums[mid]>nums[mid+1]) return mid;
+            else if(start<mid && nums[mid]<nums[mid-1]) return mid-1;
+            else if(nums[mid]>=nums[start]) start=mid+1;
+            else end=mid-1;
         }
         return -1;
     }
