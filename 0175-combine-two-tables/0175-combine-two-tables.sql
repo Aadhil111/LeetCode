25cfb69ae86a8a4ed0fdf5ca78887a7d26e4ffd9
Mohammed Aadil <111aadilofficial@gmail.com>
2025-12-18 19:34:56 +0530

    Time: 369 ms (78.61%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select Person.firstName , Person.lastName , Address.city , Address.state from Address right join Person on Person.personId = Address.personId;
